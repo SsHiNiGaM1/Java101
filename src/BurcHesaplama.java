d1b2ccbf5da86c7d350529294b98d6d366545b12
SsHiNiGaM1 <naztanyldz@gmail.com>
2022-10-12 14:56:21 +0300

    Burc Hesaplama

---
+++ src/BurcHesaplama.java
@@ -0,0 +1,153 @@
+import java.util.Scanner;
+
+
+public class BurcHesapla{
+    public static void main(String[] args){
+int month, day;
+
+System.out.print("Dogdugunuz ay: ");
+month= input.nextInt();
+
+System.out.print("Dogdugunuz gun: ");
+day= input.nextInt();
+
+if(month==1)
+{
+    if (day<22)
+    {
+        System.out.print("Oglak Burcu");
+
+    }
+    else {
+        System.out.print("Kova Burcu");
+    }
+}
+if(month==2)
+{
+    if (day<22)
+    {
+        System.out.print("Kova Burcu");
+
+    }
+    else {
+        System.out.print("Balık Burcu");
+    }
+}
+if(month==3)
+{
+    if (day<22)
+    {
+        System.out.print("Balık Burcu");
+
+    }
+    else {
+        System.out.print("Koc Burcu");
+    }
+}
+if(month==4)
+{
+    if (day<22)
+    {
+        System.out.print("Koc Burcu");
+
+    }
+    else {
+        System.out.print("Boga Burcu");
+    }
+}
+if(month==5)
+{
+    if (day<22)
+    {
+        System.out.print("Boga Burcu");
+
+    }
+    else {
+        System.out.print("Ikizler Burcu");
+    }
+}
+if(month==6)
+{
+    if (day<22)
+    {
+        System.out.print("Ikizler Burcu");
+
+    }
+    else {
+        System.out.print("Yengec Burcu");
+    }
+}
+if(month==7)
+{
+    if (day<22)
+ {
+        System.out.print("Yengec Burcu");
+
+    }
+    else {
+        System.out.print("Aslan Burcu");
+    }
+}
+if(month==8)
+{
+    if (day<22)
+    {
+        System.out.print("Aslan Burcu");
+
+    }
+    else {
+        System.out.print("Basak Burcu");
+    }
+}
+if(month==9)
+{
+    if (day<22)
+    {
+        System.out.print("Basak Burcu");
+
+    }
+    else {
+        System.out.print("Terazi Burcu");
+    }
+}
+if(month==10)
+{
+    if (day<22)
+    {
+        System.out.print("Terazi Burcu");
+
+    }
+    else {
+        System.out.print("Akrep Burcu");
+    }
+}
+if(month==11)
+{
+    if (day<22)
+    {
+        System.out.print("Akrep Burcu");
+
+    }
+    else {
+        System.out.print("Yay Burcu");
+    }
+}
+if(month==12)
+{
+    if (day<22)
+    {
+        System.out.print("Yay Burcu");
+
+    }
+    else {
+        System.out.print("Oglak Burcu");
+    }
+}
+    
+else 
+    System.out.print("Hatalı giris yaptınız.");
+
+}
+}
+
+
