4f398c0647e41ced3b23f10ebee41cda2a8711ca
SsHiNiGaM1 <naztanyldz@gmail.com>
2022-10-15 19:49:07 +0300

    Artık yıl

--- src/ArtikYilHesaplama.java
+++ src/ArtikYilHesaplama.java
@@ -5,7 +5,6 @@ public class ArtikYilHesaplama {
     int yıl;
 
     Scanner scan= new Scanner(System.in);
-System.out.print("Yıl giriniz: ");
 yıl = scan.nextInt();
 
 if ((yıl%100==0)&&(yıl%400==0))
